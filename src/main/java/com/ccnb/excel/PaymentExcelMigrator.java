863004e382672625b4b0d0a98fef49aaba49c840
Napster1010 <aditya.ndps@gmail.com>
2020-04-19 21:07:25 +0530

    changed payment excel migrator
    Signed-off-by: Napster1010 <aditya.ndps@gmail.com>

--- src/main/java/com/ccnb/excel/PaymentExcelMigrator.java
+++ src/main/java/com/ccnb/excel/PaymentExcelMigrator.java
@@ -6,10 +6,12 @@ import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.InputStream;
 import java.io.PrintWriter;
-import java.math.BigInteger;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.apache.commons.lang3.tuple.Pair;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
@@ -25,8 +27,10 @@ import com.monitorjbl.xlsx.StreamingReader;
 public class PaymentExcelMigrator {
 	public static void main(String[] args) throws Exception{
         
+		final String paymentDirectoryName = "PAYMENT";
+		SimpleDateFormat ccnbDateFormat = new SimpleDateFormat("dd-MMM-yy");
+
 		//For creating an exception Text File
-		long exceptionCount=0;
 		File file = new File(PathUtil.baseExceptionFolder + "PaymentExcelMigrationExceptionLog.txt");
 		FileWriter fw=null;
 		BufferedWriter bw = null;
@@ -46,103 +50,110 @@ public class PaymentExcelMigrator {
 		}
 		catch(Exception e){}
 		
-		File excel = new File(PathUtil.baseExcelFolder + "Payment_27072019.xlsx");
-    	InputStream is = new FileInputStream(excel);
-    	Workbook workbook = StreamingReader.builder()
-    	        .rowCacheSize(100)    
-    	        .bufferSize(4096)     
-    	        .open(is);    	  
-    	System.out.println("Excel File opened successfully!!");
+		//define the path of directory containing all the excel files to process
+		final String paymentDirectoryPath = PathUtil.baseExcelFolder + paymentDirectoryName;
+		File paymentDirectory = new File(paymentDirectoryPath);		
+
+		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();    	
+    	Session session = sessionFactory.openSession();
     	
-    	
-    	Sheet sheet = workbook.getSheetAt(0);
-    	
-    	SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
-    	
-    	Session session = null;
-    	
-    	BigInteger big = BigInteger.ZERO;
-    		
-    	session = sessionFactory.openSession();
 		long startTime = System.currentTimeMillis();
 		
-		SimpleDateFormat ccnbDateFormat = new SimpleDateFormat("dd-MMM-yy");
-		SimpleDateFormat ccnbDateFormat1 = new SimpleDateFormat("dd-MM-yyyy");
-		SimpleDateFormat ccnbDateFormat2 = new SimpleDateFormat("yyyy-MM-dd");
+		//Map containing summary about each excel file processed
+		Map<String, Pair<Long, Long>> excelSummary = new HashMap<>();		
 
-    	for(Row r: sheet)
-    	{
-        	CCNBPayment ccnbPayment = new CCNBPayment();
-        	session.clear();
-    		    		
-    		if(r.getRowNum()==0)
-    			continue;    			
-    		
-    			System.out.println();
+		for(File excel: paymentDirectory.listFiles()) {
+			if(excel.isFile()) {
+		    	InputStream is = new FileInputStream(excel);
+		    	Workbook workbook = StreamingReader.builder()
+		    	        .rowCacheSize(100)    
+		    	        .bufferSize(4096)     
+		    	        .open(is);    	  
 
-    			for(Cell c: r)
-        		{
-    				String cellValue = (c.getStringCellValue()==null)?"":c.getStringCellValue().trim();
-    				if(cellValue.isEmpty())
-    					cellValue = "0";
-        			//For setting values of all columns of the current row into bean object
-        			switch(c.getColumnIndex())
-        			{
-	        			case 0: ccnbPayment.setSource(cellValue);break;
-	        			
-	        			case 1: ccnbPayment.setOnline(strToBool(cellValue));
-	        			
-	        			case 2: ccnbPayment.setLocationCode(cellValue);break;
-	        			
-	        			case 3: ccnbPayment.setConsumerNo(cellValue);break;
-	        			
-	        			case 4: ccnbPayment.setPunchingDate((cellValue.equals("0"))?null:ccnbDateFormat.parse(cellValue));break;
-	        			
-	        			case 5: ccnbPayment.setPayDate((cellValue.equals("0"))?null:ccnbDateFormat.parse(cellValue));break;
-	        			
-	        			case 6: ccnbPayment.setAmount(cellValue);break;
-	        			
-	        			case 7: ccnbPayment.setPayMode(cellValue);break;
-	        			
-	        			case 8: ccnbPayment.setPayWindow(cellValue);break;
-	        			
-	        			case 9: ccnbPayment.setCacNo(cellValue);break;
-	        			
-	        			case 10: ccnbPayment.setDeleted(false);break;
-	        			
-	        			case 11: ccnbPayment.setPosted(true);break;
-	        			
-	        			case 12: ccnbPayment.setPostingBillMonth(cellValue);break;
-	        			
-	        			case 13: ccnbPayment.setPostingDate((cellValue.equals("0"))?null:ccnbDateFormat.parse(cellValue));break;
-        			}        			
-        			System.out.print(cellValue+" ");
-        		}  
-        		
-        		//Saving the created bean Object
-        		try
-        		{
-        			ccnbPayment.setMigrated(false);
-        			session.beginTransaction();
-	        		session.flush();
-	    			session.save(ccnbPayment);
-	    			session.getTransaction().commit();
-	    			big = big.add(BigInteger.ONE);	    	
-        		}
-        		catch(Exception e)
-        		{
-    				++exceptionCount;				
-    				writer.println();
-    				writer.println("***********EXCEPTION NUMBER " + exceptionCount + "***********" + "Occured on: " + new Date());
-    				writer.println("***********LOCATION CODE: " + ccnbPayment.getLocationCode() + "***" + "CONSUMER NUMBER: " + ccnbPayment.getConsumerNo() + "***********");
-    				writer.println("Root cause : ");
-    				e.printStackTrace(writer);				
-        			e.printStackTrace();
-        			session.getTransaction().rollback();
-        			continue;
-        		}
-        		        		    		
-    	}    	
+		    	long recordCount=0, exceptionCount=0;
+		    	System.out.println("Excel File" + excel.getName() +" opened successfully!!");		    	
+		    	
+		    	Sheet sheet = workbook.getSheetAt(0);    	
+
+		    	for(Row r: sheet)
+		    	{
+		        	CCNBPayment ccnbPayment = new CCNBPayment();
+		        	session.clear();
+		    		    		
+		    		if(r.getRowNum()==0)
+		    			continue;    			
+		    		
+		    			System.out.println();
+
+		    			for(Cell c: r)
+		        		{
+		    				String cellValue = (c.getStringCellValue()==null)?"":c.getStringCellValue().trim();
+		    				if(cellValue.isEmpty())
+		    					cellValue = "0";
+		        			//For setting values of all columns of the current row into bean object
+		        			switch(c.getColumnIndex())
+		        			{
+			        			case 0: ccnbPayment.setSource(cellValue);break;
+			        			
+			        			case 1: ccnbPayment.setOnline(strToBool(cellValue));
+			        			
+			        			case 2: ccnbPayment.setLocationCode(cellValue);break;
+			        			
+			        			case 3: ccnbPayment.setConsumerNo(cellValue);break;
+			        			
+			        			case 4: ccnbPayment.setPunchingDate((cellValue.equals("0"))?null:ccnbDateFormat.parse(cellValue));break;
+			        			
+			        			case 5: ccnbPayment.setPayDate((cellValue.equals("0"))?null:ccnbDateFormat.parse(cellValue));break;
+			        			
+			        			case 6: ccnbPayment.setAmount(cellValue);break;
+			        			
+			        			case 7: ccnbPayment.setPayMode(cellValue);break;
+			        			
+			        			case 8: ccnbPayment.setPayWindow(cellValue);break;
+			        			
+			        			case 9: ccnbPayment.setCacNo(cellValue);break;
+			        			
+			        			case 10: ccnbPayment.setDeleted(false);break;
+			        			
+			        			case 11: ccnbPayment.setPosted(true);break;
+			        			
+			        			case 12: ccnbPayment.setPostingBillMonth(cellValue);break;
+			        			
+			        			case 13: ccnbPayment.setPostingDate((cellValue.equals("0"))?null:ccnbDateFormat.parse(cellValue));break;
+		        			}        			
+		        			System.out.print(cellValue+" ");
+		        		}  
+		        		
+		        		//Saving the created bean Object
+		        		try
+		        		{
+		        			ccnbPayment.setMigrated(false);
+		        			session.beginTransaction();
+			        		session.flush();
+			    			session.save(ccnbPayment);
+			    			session.getTransaction().commit();
+			    			++recordCount;	    	
+		        		}
+		        		catch(Exception e)
+		        		{
+		    				++exceptionCount;				
+		    				writer.println();
+		    				writer.println("***********EXCEPTION NUMBER " + exceptionCount + "***********" + "FILE NAME: " + excel.getName() + "***********" + "Occured on: " + new Date());
+		    				writer.println("***********LOCATION CODE: " + ccnbPayment.getLocationCode() + "***" + "CONSUMER NUMBER: " + ccnbPayment.getConsumerNo() + "***********");
+		    				writer.println("Root cause : ");
+		    				e.printStackTrace(writer);				
+		        			e.printStackTrace();
+		        			session.getTransaction().rollback();
+		        			continue;
+		        		}		        		        		    		
+		    	}    					
+		    	//add an entry in the summary map for the file
+		    	excelSummary.put(excel.getName(), Pair.of(recordCount, exceptionCount));
+				is.close();
+				workbook.close();		    
+			}
+		}
+		
 		session.close();
 		
 		long endTime = System.currentTimeMillis();
@@ -154,14 +165,23 @@ public class PaymentExcelMigrator {
     	System.out.println();
     	System.out.println("MIGRATION OF PAYMENT_EXCEL SUCCESSFULLY DONE!!!!");
     	System.out.println();
-    	System.out.println(big.toString() + " ROWS SUCCESSFULLY INSERTED INTO THE DATABASE!!!!");    	    	
-		System.out.println(exceptionCount + " EXCEPTIONS CAUGHT !! PLEASE REFER EXCEPTION LOG FOR MORE DETAILS!!");		
+    	System.out.println("SUMMARY:\n");
+    	//calculate total records inserted and the total no. of exceptions
+    	long totalRecordsInserted=0, totalExceptions=0;    	
+    	for(Map.Entry<String, Pair<Long, Long>> fileSummary: excelSummary.entrySet()) {
+    		System.out.println("File Name: " + fileSummary.getKey() + ", " + 
+    				"Records successfully inserted: " + fileSummary.getValue().getLeft() + ", " + 
+    				"Excpetions: " + fileSummary.getValue().getRight());
+    		totalRecordsInserted += fileSummary.getValue().getLeft();
+    		totalExceptions += fileSummary.getValue().getRight();
+    	}    	
+    	System.out.println("\n Total Records inserted: " + totalRecordsInserted);
+    	System.out.println("Total Exceptions caught: " + totalExceptions);    	
+
 		System.out.println("Time Elapsed: " + minutes + " Minutes " + seconds + " Seconds");	
 		session.close();
 		sessionFactory.close();
-		is.close();
 		writer.close();
-		workbook.close();		    
 	}
 
 	private static boolean strToBool(String str) {
